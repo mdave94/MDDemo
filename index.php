1ed890c3f001fa3c7eeaddc790d74c9393a53bf4
David Molnár <molnardavid2002@gmail.com>
2018-10-20 21:09:06 +0200

    Start

--- index.php
+++ index.php
@@ -5,10 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>MDavid</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="welcomePageCSS.css" />
     <script src="main.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+<div class="container">
+
+
+                <div class="row align-items-center">
+                        <div class="welcomeText col-md col-12" >
+                            Hello, I'm David Molnar
+                        </div>    
+                </div>
+            
+                <div class="row align-items-center">
+                            <div class="showCV col-5 ">
+                                My CV:
+                            </div>
+
+                            <div class="myProjects col-md col-7">
+                                My Projects:
+                            </div>  
+                <div>
+</div>
+</div>
+<div class="row " >
+CV LINKJE
+</div>
 </body>
 </html>
